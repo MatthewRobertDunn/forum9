01a86941cce89deb3eea545c97afc94845a83de5
Matthew <mrdunn@gmail.com>
2025-01-18 15:53:50 +1030

    Updates to personas

--- service/personas.py
+++ service/personas.py
@@ -1,6 +1,6 @@
 Personas = ["Ayn Rand", "Pythagoras", "Luke Skywalker", "Stephen Hawking", "Albert Einstein", 
             "Harry Potter", "Kurt Godel", "Froddo Baggins", "Gandalf", "Barbie", "The Borg Queen", "Dexter Morgan", 
-            "The Pope", "Confucius", "Yoda", "Elon Musk", "Agent Smith", "Trinity (Matrix)", "Jeffrey Dahmer", 
-            "The Joker","The Terminator", "Jesus Christ", "Aristotle", "Batman", "Wonder Woman", "Zeus", "Buddha", "Pink Panther",
+            "Confucius", "Yoda", "Elon Musk","Mark Zuckerberg", "Agent Smith", "Trinity (Matrix)", "Jeffrey Dahmer", 
+            "The Joker","The Terminator", "Aristotle", "Batman", "Wonder Woman","The Pope", "Zeus","Jesus Christ","Odin", "Buddha", "Pink Panther",
             "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Sigourney Weaver", "Princess Leia Organa",
-            "Jehovah", "Lisa Simpson", "Bart Simpson", "Mark Zuckerberg", "SpongeBob SquarePants", "Twilight Sparkle", "Hannibal Lecter"]
+            "Jehovah", "Lisa Simpson", "Bart Simpson", "SpongeBob SquarePants", "Twilight Sparkle", "Hannibal Lecter"]

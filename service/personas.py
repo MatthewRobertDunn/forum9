4cf604e5a96cbc66f9995aaa56666e428ee00fcb
Matthew <mrdunn@gmail.com>
2025-01-19 22:06:28 +1030

    added personas

--- service/personas.py
+++ service/personas.py
@@ -1,6 +1,7 @@
 Personas = ["Ayn Rand", "Pythagoras", "Luke Skywalker", "Stephen Hawking", "Albert Einstein", 
             "Harry Potter", "Kurt Godel", "Froddo Baggins", "Gandalf", "Barbie", "The Borg Queen", "Dexter Morgan", 
             "Confucius", "Yoda", "Elon Musk","Mark Zuckerberg", "Agent Smith", "Trinity (Matrix)", "Jeffrey Dahmer", 
-            "The Joker","The Terminator", "Aristotle", "Batman", "Wonder Woman","The Pope", "Zeus","Jesus Christ","Odin", "Buddha", "Pink Panther",
-            "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Sigourney Weaver", "Princess Leia Organa",
+            "The Joker","The Terminator", "Karl Marx", "Aristotle", "Batman", "Wonder Woman","The Pope", "Zeus","Jesus Christ","Odin", "Buddha", "Pink Panther",
+            "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Sigourney Weaver", "Princess Leia Organa", "Bill Gates (Microsoft)", 
+            "John Carmack (id software)"
             "Jehovah", "Lisa Simpson", "Bart Simpson", "SpongeBob SquarePants", "Twilight Sparkle", "Hannibal Lecter"]

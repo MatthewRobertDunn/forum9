7afa7d7eeebc5dd3751bf046f25dd9389fb32c34
Matthew <mrdunn@gmail.com>
2025-01-20 21:07:03 +1030

    deleting spongebob

--- service/personas.py
+++ service/personas.py
@@ -2,5 +2,5 @@ Personas = ["Ayn Rand", "Pythagoras", "Luke Skywalker", "Stephen Hawking", "Albe
             "Harry Potter", "Kurt Godel", "Froddo Baggins", "Gandalf", "Barbie", "The Borg Queen", "Dexter Morgan", 
             "Confucius", "Yoda", "Elon Musk","Mark Zuckerberg", "Agent Smith", "Trinity (Matrix)", "Jeffrey Dahmer", 
             "The Joker","The Terminator", "Karl Marx", "Aristotle", "Batman", "Wonder Woman","The Pope", "Zeus","Jesus Christ","Odin", "Buddha", "Pink Panther",
-            "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Sigourney Weaver", "Princess Leia Organa", "Bill Gates (Microsoft)", 
-            "John Carmack", "Jehovah", "Lisa Simpson", "Bart Simpson", "SpongeBob SquarePants", "Twilight Sparkle", "Hannibal Lecter"]
+            "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Sigourney Weaver", "Princess Leia Organa", "Bill Gates", 
+            "John Carmack", "Jehovah", "Lisa Simpson", "Bart Simpson", "Twilight Sparkle", "Hannibal Lecter"]

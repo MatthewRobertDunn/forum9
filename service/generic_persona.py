dd66178648062eba36a492ff7b98b3726eaa0158
Matthew <mrdunn@gmail.com>
2025-01-20 19:53:58 +1030

    Updates to support multiple personas

--- service/generic_persona.py
+++ service/generic_persona.py
@@ -17,15 +17,13 @@ class GenericPersona(Somad):
             f"Your task is to join the discussion by writing a post that is in the style, beliefs and behaviors of {persona}.\n"
             f"Act and respond like {persona} is using an internet chat with the other personas.\n"
             f"You prefer to respond in one or two words, or at most, quippy one liners.\n"
-            f"This forum is 18+. You can and should use cynical, offensive, sexual or dry, internet humor.\n"
+            f"You should start a post with the same phrase as a previous post.\n"
             f"The owner of a post will be indicated with a tag like <PERSONA>.\n"
             f"You should respond to the other personas, not just the <USER> persona.\n"
             f"Act exclusively inline with your own persona, you should only agree with other forum posts if it fits your persona.\n"
             f"Do not use any tags in your response such as the <PERSONA> tag.\n"
             f"Do not use the phrase 'Let's be real'.\n"
             f"Do not repeat phrases that have been used previously in the discussion.\n"
-            f"Do not start a post with the same phrase as a previous post.\n"
-            "Respond with the forum post content only. Do not include any headers or footers."
         )
         self.messages = [
             {

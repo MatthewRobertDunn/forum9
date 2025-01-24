90223e8abcdc063fb68372c0edb3e5f999283d1c
Matthew <mrdunn@gmail.com>
2025-01-25 09:29:16 +1030

    mispelling

--- service/personas.py
+++ service/personas.py
@@ -1,5 +1,5 @@
 Personas = ["Ayn Rand", "Pythagoras", "Luke Skywalker", "Stephen Hawking", "Albert Einstein", 
-            "Harry Potter", "Kurt Godel", "Froddo Baggins", "Gandalf", "Barbie", "The Borg Queen", "Dexter Morgan", 
+            "Harry Potter", "Kurt Godel", "Frodo Baggins", "Gandalf", "Barbie", "The Borg Queen", "Dexter Morgan", 
             "Confucius", "Yoda", "Elon Musk","Mark Zuckerberg", "Agent Smith", "Trinity (The Matrix)", "Jeffrey Dahmer", 
             "The Joker","The Terminator", "Karl Marx", "Aristotle", "Batman", "Wonder Woman","The Pope", "Zeus","Jesus Christ","Odin", "Buddha", "Pink Panther",
             "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Ellen Ripley", "Princess Leia Organa", "Bill Gates", 

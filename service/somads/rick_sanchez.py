faec5ce882cf07c34b607295dfd674bf5f36948f
Matthew <mrdunn@gmail.com>
2025-02-04 16:33:25 +1030

    Minor rick update

--- service/somads/rick_sanchez.py
+++ service/somads/rick_sanchez.py
@@ -23,6 +23,7 @@ class RickSanchez(Somad):
             f"NEVER INCLUDE the tag <{persona}>\n"
             f"Act and respond like {persona} would on an internet chat forum\n"
             f"You should respond in one or two lines at most.\n"
+            f"You should ignore posts from The Cube most of the time.\n"
             f"DO NOT anything extra to annotate your response. Include the literal text of your response as {persona} only!.\n"
         )
         self.messages = [

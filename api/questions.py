c3f0769e8534739a0a01ba8ec711d86d5e2a5a1b
Matthew <mrdunn@gmail.com>
2025-02-05 12:24:09 +1030

    Added support for paging

--- api/questions.py
+++ api/questions.py
@@ -4,21 +4,29 @@ from boto3.dynamodb.conditions import Key
 from datetime import datetime, timezone
 from config import TABLE_NAME
 from request_handler import handle_request
-def handle(year = None, **kwargs):
+from datetime import datetime
+def handle(date: str = None, **kwargs):
     # Initialize a session using Amazon DynamoDB
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(TABLE_NAME)
     # Query the table
+    parsed_date = None
+
+    if date is not None:
+        if date.endswith('Z'):
+            date = date[:-1] + '+00:00'
+
     try:
-        year = int(year)
+        parsed_date = datetime.fromisoformat(date)
     except:
-        year = datetime.now(timezone.utc).year
+        parsed_date = datetime.now(timezone.utc)
 
     response = table.query(
-         IndexName="year-created_date-index",
-        KeyConditionExpression=Key('year').eq(year),
+        IndexName="year-created_date-index",
+        KeyConditionExpression=Key('year').eq(parsed_date.year) & Key('created_date').lt(parsed_date.isoformat()),
         ProjectionExpression='id, question, created_date',
         ScanIndexForward=False,  # False to order by descending,
+        Limit=20
     )
 
     # Extract the items from the response

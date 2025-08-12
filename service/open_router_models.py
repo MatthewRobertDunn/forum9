19e827c54d2c01e21b67dbc025d945ae7108cc03
Matthew <mrdunn@gmail.com>
2025-08-12 21:31:36 +0930

    Minor update to ordering

--- service/open_router_models.py
+++ service/open_router_models.py
@@ -2,18 +2,18 @@
 # The general models of the collection
 GeneralModels = [
     "qwen/qwen3-235b-a22b:free",
+    "deepseek/deepseek-chat-v3-0324:free",
     "deepseek/deepseek-r1-0528:free",
     "meta-llama/llama-3.1-405b-instruct:free",
     "meta-llama/llama-3.3-70b-instruct:free",
-    "qwen/qwen3-4b:free",
-    "qwen/qwen3-coder:free",
     "qwen/qwen-2.5-72b-instruct:free",
-    "deepseek/deepseek-chat-v3-0324:free",
+    "qwen/qwen3-coder:free",
     "deepseek/deepseek-r1-distill-llama-70b:free",
-    "google/gemma-3-27b-it:free",
     "tngtech/deepseek-r1t2-chimera:free",
+    "google/gemma-3-27b-it:free",
     "cognitivecomputations/dolphin3.0-r1-mistral-24b:free",
     "mistralai/mistral-small-3.2-24b-instruct:free",
+    "qwen/qwen3-4b:free",
 ]
 
 # The fastest models of the collection, good for dumb characters, formatting.

92bef6113d7af52aef3d8934c06f832a58067682
Matthew <mrdunn@gmail.com>
2025-08-14 18:00:53 +0930

    small rename

--- service/dyanmodb_repo.py
+++ service/dyanmodb_repo.py
@@ -2,11 +2,11 @@ import boto3
 from config import TABLE_NAME
 
 
-def insert_thread(post):
+def insert_thread(thread):
     # Initialize the DynamoDB resource
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(TABLE_NAME)
-    table.put_item(Item=post)
+    table.put_item(Item=thread)
     print("Item inserted successfully!")
 
 

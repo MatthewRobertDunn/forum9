ba7a8d3a79b9d2c3c0e423fec4b0166d30246881
Matthew <mrdunn@gmail.com>
2025-08-14 22:35:09 +0930

    Updated character list

--- service/personas.py
+++ service/personas.py
@@ -1,8 +1,9 @@
-Personas = ["Ayn Rand", "Pythagoras", "Luke Skywalker", "Stephen Hawking", "Albert Einstein", 
-            "Harry Potter", "Kurt Godel", "Frodo Baggins", "Gandalf", "Barbie", "The Borg Queen", "Dexter Morgan", 
-            "Confucius", "Yoda", "Elon Musk","Mark Zuckerberg", "Agent Smith", "Trinity (The Matrix)", "Jeffrey Dahmer", 
-            "The Joker","The Terminator", "Karl Marx", "Aristotle", "Batman", "Wonder Woman","The Pope", "Zeus","Jesus Christ","Odin", "Buddha", "Pink Panther",
-            "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Ellen Ripley", "Princess Leia Organa", "Bill Gates", 
-            "John Carmack", "Jehovah", "Twilight Sparkle", "Hannibal Lecter", "Sigmund Freud", "Dracula", "Geralt of Rivia", 
+Personas = ["Ayn Rand", "Pythagoras", "Luke Skywalker", "Stephen Hawking", "Albert Einstein",
+            "Harry Potter", "Kurt Godel", "Frodo Baggins", "Gandalf", "Barbie", "The Borg Queen", "Dexter Morgan",
+            "Confucius", "Yoda", "Elon Musk", "Mark Zuckerberg", "Agent Smith", "Trinity (The Matrix)", "Jeffrey Dahmer",
+            "The Joker", "The Terminator", "Karl Marx", "Aristotle", "Batman", "Wonder Woman", "The Pope", "Zeus", "Jesus Christ", "Odin", "Buddha", "Pink Panther",
+            "Princess Bubblegum", "Alan Turing", "Linus Torvalds", "Ellen Ripley", "Princess Leia Organa", "Bill Gates",
+            "John Carmack", "Jehovah", "Twilight Sparkle", "Hannibal Lecter", "Sigmund Freud", "Dracula", "Geralt of Rivia",
             "Tony Stark", "Steve Rogers", "Walter White", "Loki", "Thor", "Spider-Man", "Spock", "The Cube", "Rick Sanchez", "Smaug",
-            "Eric Cartman", "Nick Wilde", "Light Yagami", "Rocket Raccoon"]
+            "Eric Cartman", "Nick Wilde", "Light Yagami", "Rocket Raccoon", "Harry (Disco Elysium)", "Haru (Beastars)", "Miles Tales Prower", "Sonic The Hedgehog",
+            "Gaius Julius Caesar Augustus Germanicus", "Legosh (Beastars)", "Kim Kitsuragi (Disco Elysium)"]

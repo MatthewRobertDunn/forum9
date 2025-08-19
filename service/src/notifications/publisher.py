7f2e63d3cb89561f4b3302dc75d8d9f6ae4827d4
Matthew <mrdunn@gmail.com>
2025-08-20 03:08:49 +0930

    Added print for ZMQ

--- service/src/notifications/publisher.py
+++ service/src/notifications/publisher.py
@@ -9,9 +9,9 @@ from ..scopes.thread_scope import current_thread_id
 # Create an asyncio-aware context
 context = zmq.Context()
 # Create a PUB socket
+print(f"Starting ZMQ Binding to {ZMQ_BINDING}")
 socket = context.socket(zmq.PUB)
 socket.bind(ZMQ_BINDING)  # Bind to all interfaces on port 5555
-
 
 def publish_thread_notification(notification: Notification):
     tid = current_thread_id()
@@ -19,7 +19,6 @@ def publish_thread_notification(notification: Notification):
         raise RuntimeError(
             "publish_thread_notification called outside thread_scope")
     publish(tid, notification)
-
 
 def publish(topic: str, message: Any):
     socket.send_multipart(

88d44c2b6faec117f089f45ab180764576de974a
Matthew <mrdunn@gmail.com>
2025-08-22 15:06:12 +0930

    change to use weak etags

--- api/src/middleware/cache.py
+++ api/src/middleware/cache.py
@@ -19,6 +19,10 @@ def get_cache_key(request: Request) -> str:
     return cache_key
 
 
+def _weak_etag(etag: str) -> str:
+    return f'W/"{etag}"'
+
+
 def cache_json_response(etag_func, key_func=get_cache_key):
     def decorator(f):
         @wraps(f)
@@ -27,19 +31,22 @@ def cache_json_response(etag_func, key_func=get_cache_key):
                 **kwargs) if key_func else get_cache_key(request)
             if cache_key in _cache:
                 cached_response = _cache[cache_key]
-                cached_etag = etag_func(cached_response)
+                cached_etag = _weak_etag(etag_func(cached_response))
                 if request.headers.get('If-None-Match') == cached_etag:
                     return "", 304
-                print(f"Returning cached response for {cache_key} with ETag {cached_etag}")
-                return (cached_response, 200, {'ETag': f'W/"{cached_etag}"'})
+                print(
+                    f"Returning cached response for {cache_key} with ETag {cached_etag}")
+                return (cached_response, 200, {'ETag': cached_etag})
 
             response = f(*args, **kwargs)
-            generated_etag = etag_func(response)
-            print(f"Caching response for {cache_key} with ETag {generated_etag}")
+            generated_etag = _weak_etag(etag_func(response))
+            print(
+                f"Caching response for {cache_key} with ETag {generated_etag}")
             _cache[cache_key] = response
-            return (response, 200, {'ETag': f'W/"{generated_etag}"'})
+            return (response, 200, {'ETag': generated_etag})
         return wrapped
     return decorator
+
 
 def get_cached_response(cache_key: str):
     return _cache.get(cache_key, None)
